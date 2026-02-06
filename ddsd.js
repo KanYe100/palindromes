0322456b00acff05872a87842ac1659244d08d91
KanYe100 <defuyeet12@gmail.com>
2026-02-06 14:23:12 -0800

    planindrome checkers

---
+++ ddsd.js
@@ -0,0 +1,13 @@
+let word = prompt("Palindrome checker")
+let reverse = ""
+let noSpace = word.replace(/\s+/g, "");
+
+    for (let i = noSpace.length - 1; i >= 0; i--) {
+    reverse += noSpace[i];
+    }
+    if (reverse = noSpace) {
+        console.log("True");
+    }
+    else {
+        console.log("False");
+    };
